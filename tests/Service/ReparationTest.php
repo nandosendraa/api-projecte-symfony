99f423f95be7062c81f75acee740d849292d17e3
Nando <nandet2003@gmail.com>
2023-02-20 12:52:41 +0100

    add test (not working)

--- tests/Service/ReparationTest.php
+++ tests/Service/ReparationTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Service;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\Reparation;
+use App\Repository\UserRepository;
 
 class ReparationTest extends ApiTestCase
 {
@@ -14,9 +15,25 @@ class ReparationTest extends ApiTestCase
         );
 
         $this->assertResponseIsSuccessful();
-        //$this->assertMatchesResourceCollectionJsonSchema(Reparation::class);
+        $this->assertMatchesResourceCollectionJsonSchema(Reparation::class);
 
         $this->assertCount(30, $response->toArray());
 
+    }
+
+    public function testPostValidData():void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(["username" => "user"]);
+
+        $response = $this->client->request('POST', '/api/tweets',
+            [
+                'headers' => ["Accept: application/json"],
+                'json' => [
+                    'text' => 'Proves',
+                    'author' => '/api/users/' . $user->getId(),
+                ]
+            ]
+        );
     }
 }
